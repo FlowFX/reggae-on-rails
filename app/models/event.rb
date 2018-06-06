d853cc1ca7d7267d98772117cb0cbc27d983f5d1
Florian Posdziech <hallo@flowfx.de>
2018-06-06 08:27:29 +0200

    Validate and test the model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   validates :title, :date, presence: true
+  validates :title, length: {minimum: 5}
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'event requires title and date' do
+    event = Event.new
+    assert event.invalid?
+    assert event.errors[:title].any?
+    assert event.errors[:date].any?
+  end
+
+  test 'event title require at least 5 characters' do
+    event = events(:one)
+    assert event.valid?
+
+    event.title = '1234'
+    assert event.invalid?
+  end
 end
