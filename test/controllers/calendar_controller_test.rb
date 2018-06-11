a628bc6b8159db0f137edc3b39ab671a5ef85c73
Florian Posdziech <hallo@flowfx.de>
2018-06-11 08:40:08 +0200

    Improve layout

--- test/controllers/calendar_controller_test.rb
+++ test/controllers/calendar_controller_test.rb
@@ -4,7 +4,6 @@ class CalendarControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get calendar_index_url
     assert_response :success
-    assert_select 'h1', 'Eventos'
     assert_select 'td', 'Jahshua Soundman Birthday Bash'
   end
 
