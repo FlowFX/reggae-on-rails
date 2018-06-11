e308a805c7ef99d3b0ebb916a8b797a01c6f27c8
Florian Posdziech <hallo@flowfx.de>
2018-06-11 08:36:22 +0200

    Use assert_select in controller test

--- test/controllers/calendar_controller_test.rb
+++ test/controllers/calendar_controller_test.rb
@@ -4,6 +4,8 @@ class CalendarControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get calendar_index_url
     assert_response :success
+    assert_select 'h1', 'Eventos'
+    assert_select 'td', 'Jahshua Soundman Birthday Bash'
   end
 
 end
