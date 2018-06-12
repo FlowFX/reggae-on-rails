5ca67461ce76f4d797335deb4faea5d441508124
Florian Posdziech <hallo@flowfx.de>
2018-06-12 08:12:19 +0200

    Add Venue as foreign key to Event model

---
+++ db/migrate/20180612061021_add_venue_ref_to_events.rb
@@ -0,0 +1,5 @@
+class AddVenueRefToEvents < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :events, :venue, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180612055739) do
+ActiveRecord::Schema.define(version: 20180612061021) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20180612055739) do
     t.string "link_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "venue_id"
+    t.index ["venue_id"], name: "index_events_on_venue_id"
   end
 
   create_table "venues", force: :cascade do |t|
