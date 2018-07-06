e40b09c2f4145e75957b3313065c6a6f7d8b0320
Florian Posdziech <hallo@flowfx.de>
2018-07-06 07:41:16 +0200

    Add User model

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ ruby '2.5.1'
 
 gem 'rails', '~> 5.2'
 gem 'puma', '~> 3.7'
+gem 'bcrypt'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180612061021) do
+ActiveRecord::Schema.define(version: 2018_07_06_053819) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20180612061021) do
     t.datetime "updated_at", null: false
     t.integer "venue_id"
     t.index ["venue_id"], name: "index_events_on_venue_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "venues", force: :cascade do |t|
