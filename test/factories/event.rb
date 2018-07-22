d25c6b70a1c7faf77a6c5a2924f36eddc406c390
Florian Posdziech <hallo@flowfx.de>
2018-07-22 19:38:59 +0200

    Show events on home page aka calendar#index

--- test/factories/event.rb
+++ test/factories/event.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  sequence :event_title do |n|
+    "Event #{n}"
+  end
+end
+
+FactoryBot.define do
   factory :event do
-    title 'Birthday Bash'
+    title { generate(:event_title) }
     date Date.tomorrow
     description 'A wicked party.'
   end
