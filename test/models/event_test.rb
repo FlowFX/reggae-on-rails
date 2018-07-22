8ca234249816df6aac30d9f8311952f818e40915
Florian Posdziech <hallo@flowfx.de>
2018-07-22 12:24:33 +0200

    Add model tests for Event

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,25 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @event = create(:event)
+  end
+
+  test 'valid event' do
+    assert @event.valid?
+  end
+
+  test 'invalid without title' do
+    @event.title = nil
+    assert_not @event.valid?
+    assert_not_nil @event.errors[:title]
+  end
+
+  test 'invalid without date' do
+    @event.date = nil
+    assert_not @event.valid?
+    assert_not_nil @event.errors[:date]
+  end
 end
