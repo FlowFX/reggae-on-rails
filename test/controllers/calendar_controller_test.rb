81e921589d3b07c7960fba9ca373a24d3e17644c
Florian Posdziech <hallo@flowfx.de>
2018-07-22 13:30:30 +0200

    Let calendar controller show events

--- test/controllers/calendar_controller_test.rb
+++ test/controllers/calendar_controller_test.rb
@@ -1,8 +1,27 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class CalendarControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @tomorrow = create(:event, title: "Tomorrow's party", date: Date.tomorrow)
+    @today = create(:event, title: "Today's party", date: Time.zone.today)
+  end
+
   test 'should get index' do
     get root_url
     assert_response :success
+  end
+
+  test 'calendar index has events' do
+    get root_url
+    assert_not_nil assigns(:events)
+  end
+
+  test 'events are shown in ascending temporal order' do
+    get root_url
+
+    assert_equal assigns(:events)[0], @today
+    assert_equal assigns(:events)[1], @tomorrow
   end
 end
