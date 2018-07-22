fb06724f20c7d2daa1f9cda7d41ee438b6aec2fa
Florian Posdziech <hallo@flowfx.de>
2018-07-22 19:38:59 +0200

    Show events on home page aka calendar#index

--- test/system/visitor_test.rb
+++ test/system/visitor_test.rb
@@ -15,8 +15,18 @@ class VisitorTest < ApplicationSystemTestCase
     assert_selector 'h1', text: 'Reggae CDMX'
 
     # and also the title of tonight's event.
+    assert page.has_content?(@events.first.title)
+
     # She click's on tonight's event and gets shown the details page.
+    click_on(@events.first.title)
+
     # There she sees the event's title, date and description.
-    # Happily, she returns to the main calendar to look for more events in the future.
+    assert page.has_content?(@events.first.title)
+    assert page.has_content?(@events.first.date)
+    assert page.has_content?(@events.first.description)
+
+    # Happily, she returns to the main calendar to
+    # look for more events in the future.
+    # TODO: finish test
   end
 end
