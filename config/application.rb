f7cf05b47b715114d538325756236805b9303cee
Florian Posdziech <hallo@flowfx.de>
2018-07-30 16:25:01 +0200

    Configure i18n and default locale
    Close: #73

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,13 @@ module ReggaeOnRails
     # Use plain JavaScript
     config.generators.javascript_engine = :js
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
+    # Settings in config/environments/* take precedence over those specified
+    # here. Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Configure i18n
+    config.i18n.available_locales = %i[en es]
+    config.i18n.default_locale = :es
   end
 end
