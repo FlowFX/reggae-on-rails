68d39ab26f44c4b0d34ba4e0c1f07299c681a0b2
Florian Posdziech <hallo@flowfx.de>
2018-08-07 17:07:41 +0200

    Add seed.rb file for events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+# TODO: use Faker lorem to add descriptions
+# TODO: use Faker text to use for titles
+events = Event.create(
+  [
+    { title: 'Last week',  date: (Time.zone.today - 7.days), description: '' },
+    { title: 'Today',      date: Time.zone.today, description: '' },
+    { title: 'Tomorrow',   date: (Time.zone.today + 1.day), description: '' },
+    { title: 'Next week',  date: (Time.zone.today + 7.days), description: '' },
+    { title: 'Next month', date: (Time.zone.today + 28.days), description: '' },
+    { title: 'Next month', date: (Time.zone.today + 31.days), description: '' },
+    { title: 'Next month', date: (Time.zone.today + 34.days), description: '' },
+    { title: 'Next year',  date: (Time.zone.today + 360.days), description: '' }
+  ]
+)
