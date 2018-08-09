1998eb2065c4efc17f42d87df9a5bbc0e02bbc78
Florian Posdziech <hallo@flowfx.de>
2018-08-09 15:46:25 +0200

    Handle 404 not found errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
+
+  def route_not_found
+    render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'calendar#index' # shortcut for the above
+
+  # all your other routes
+  match '*unmatched', to: 'application#route_not_found', via: :all
 end
