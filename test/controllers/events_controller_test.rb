fadd55e7904c3918e41c78f2bbb52bfc627adb97
Florian Posdziech <hallo@flowfx.de>
2018-08-10 19:58:02 +0200

    Require authentication to create and edit events

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
+    @user = create(:user)
+
     @event = create(:event)
     @event_params = {
       event: {
@@ -16,34 +20,83 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     create(:event, :past)
   end
 
-  test 'should get index' do
+  ### ANONYMOUS USER ###
+  test '#index redirects to sign in page' do
+    get events_url
+    assert_redirected_to new_user_session_path
+  end
+
+  test '#new redirects to sign in page' do
+    get new_event_url
+    assert_redirected_to new_user_session_path
+  end
+
+  test '#create should be forbidden' do
+    post events_url, params: @event_params
+    assert_redirected_to new_user_session_path
+  end
+
+  test '#edit redirects to sign in page' do
+    get edit_event_url(@event)
+    assert_redirected_to new_user_session_path
+  end
+
+  test '#update is forbidden' do
+    patch event_url(@event), params: @event_params
+    assert_redirected_to new_user_session_path
+  end
+
+  test '#show should show event' do
+    get event_url(@event)
+    assert_response :success
+  end
+
+  test '#show should provide all correct template variables' do
+    get event_url(@event)
+
+    assert_equal assigns(:event).title,       @event.title
+    assert_equal assigns(:event).date,        @event.date
+    assert_equal assigns(:event).description, @event.description
+  end
+
+  ### AUTHENTICATED USER ###
+  test 'can get index' do
+    sign_in @user
+
     get events_url
     assert_response :success
   end
 
   test 'index has events' do
+    sign_in @user
+
     get events_url
     assert_not_nil assigns(:events)
   end
 
   test 'index shows events in descending temporal order' do
+    sign_in @user
     get events_url
 
     assert assigns(:events).first.date > assigns(:events).last.date
   end
 
   test 'index shows all events ever' do
+    sign_in @user
     get events_url
 
     assert_equal assigns(:events).length, Event.all.count
   end
 
   test 'should get new' do
+    sign_in @user
     get new_event_url
+
     assert_response :success
   end
 
   test 'should create event' do
+    sign_in @user
     assert_difference('Event.count') do
       post events_url, params: @event_params
     end
@@ -52,64 +105,21 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     assert flash[:notice].present?
   end
 
-  test 'event requires title on create' do
-    # GIVEN incomplete event parameters
-    @event_params[:event][:title] = nil
-
-    # WHEN creating a new event
-    # THEN nothing happens
-    assert_difference('Event.count', 0) do
-      post events_url, params: @event_params
-    end
-    assert assigns(:event).errors.present?
-  end
-
-  test 'event requires title on update' do
-    # GIVEN incomplete event parameters
-    @event_params[:event][:title] = nil
-
-    # WHEN updating an event
-    # THEN nothing happens
-    assert_difference('Event.count', 0) do
-      patch event_url(@event), params: @event_params
-    end
-    assert assigns(:event).errors.present?
-  end
-
-  test 'event requires date' do
-    @event_params[:event][:date] = nil
-
-    assert_difference('Event.count', 0) do
-      post events_url, params: @event_params
-    end
-  end
-
-  ### SHOW ###
-  test 'should show event' do
-    get event_url(@event)
-    assert_response :success
-  end
-
-  test 'should provide all correct template variables' do
-    get event_url(@event)
-
-    assert_equal assigns(:event).title,       @event.title
-    assert_equal assigns(:event).date,        @event.date
-    assert_equal assigns(:event).description, @event.description
-  end
-
   ### UPDATE ###
   test 'should get edit' do
+    sign_in @user
     get edit_event_url(@event)
     assert_response :success
   end
 
   test 'should update event' do
+    sign_in @user
     patch event_url(@event), params: @event_params
     assert_redirected_to event_url(@event)
   end
 
   test 'should destroy event' do
+    sign_in @user
     assert_difference('Event.count', -1) do
       delete event_url(@event)
     end
